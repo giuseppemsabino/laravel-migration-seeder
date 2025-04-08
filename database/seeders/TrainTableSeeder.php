e1caa7ef9b9d39c6c01ccfec80ea965e63ad5161
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-08 17:32:42 +0200

    ati fake creati

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +14,21 @@ class TrainTableSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        
+        for($i = 0; $i < 20;$i++){
+
+            $newTrain = new Train();
+    
+            $newTrain->train_company = $faker->company(); 
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->unique()->regexify('[A-Z]{2}[0-9]{4}');
+            $newTrain->train_cars = $faker->numberBetween(1, 20);
+            $newTrain->is_on_time = $faker->boolean();
+            $newTrain->is_cancelled = $faker->boolean();
+    
+            $newTrain->save();
+        }
     }
 }
